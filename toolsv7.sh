c8e7c9ca06feb08ceac699e683f6feae8e344278
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-05-03 16:45:54 +0700

    Add files via upload

---
+++ toolsv7.sh
@@ -0,0 +1,180 @@
+#!/system/xbin/bash
+#mau ngapain ngerecode lu yah?
+#mikir cok pake otak
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+clear
+echo $i"Sebutkan Nama Anda Untuk masuk"
+echo $i"NAMANYA SEKOCAK KOCAKNYA KOCAK :v"
+read -p "NAMAMU SIAPA COK :v = " nama
+if [ $nama = $nama ]
+then
+toilet GOOD | lolcat
+echo $ku"HAI" $nama" Lo Mirip Mbah Sugiono Bigbos Ganteng"
+sleep 2
+echo $bi"HAI" $nama" Lo Mirip Mbah Sugiono Bigbos Ganteng"
+sleep 2
+echo $ku"Gue ejek sekali lagi tools akan mulai"
+sleep 2
+echo $pu"HAI" $nama" Lo Mirip Sekali Sama Mbah Sugiono Bigbos Ganteng"
+sleep 3
+fi
+
+echo $me"TOOLSVERSI KE 7 AKAN DIMULAI"
+echo $i"dalam 2 detik lagi"
+echo
+sleep 3
+clear
+echo '''\a
+satu (1) __________
+        /          |
+       /    _      |
+      /_____||     |
+             |     |
+             |     |
+             |     |
+             |     |
+             |     |
+     ________|     |_____
+     |                  |
+     |__________________|'''
+echo
+sleep 2
+clear
+echo '''\a
+dua (2)
+       _____________
+      /             \
+     /    ____       \
+    /    /    \      |
+   |     |   /      /
+   |_____|  /      /
+           /      /
+          /      /
+         /      /
+      __/      ______________
+      |      		    |
+      |_____________________|'''
+echo
+sleep 2
+clear
+echo
+echo $me"()()()()()()()()()()()()()()()()()()()()()()()()"
+echo
+echo $ku"AUTHOR BIGBOS STARLA" $i"(GREEN DRAGON)"
+echo $bi"Friends Arul-Mr.ghost-Bang Hans"
+echo $cy"Tools Versi ke 7"
+echo $pu"Team Cyb3r Pop1ce +62"
+echo
+echo $me"()()()()()()()()()()()()()()()()()()()()()()()()"
+echo
+echo $bi"###########################################"
+echo $ku"MENU TOOLS SILAHKAN DIPILIH SALAH SATU"
+echo
+echo $pu"1. tools sederhana"
+echo $i"2. Tools TuanB4dut"
+echo $i"3. install bahan bahan biar semua work"
+echo $i"4. Tools Bajinganv6"
+echo $i"0. KLUAR DARI TOOLSv7"
+echo
+echo $pu"Silahkan Dipilih Toolsnya"
+echo
+echo $ku"Pilih dengan 1 jangan 01 atau 02"
+echo $bi"gue tinggal Yak mas hehehhe"
+echo
+read -p "GreenDragon : " pilihan
+if [ $pilihan = 1 ]
+then
+clear
+figlet GREEN | lolcat
+figlet DRAGON | lolcat
+echo $bi"Gue Tinggal Dulu Pamit Yah" $nama
+echo
+sleep 3
+pkg install python
+pkg install git
+git clone https://github.com/yordankeres123/sederhanatools
+ls
+cd sederhanatools
+sh sederhana.sh
+fi
+
+if [ $pilihan = 2 ]
+then
+clear
+toilet -f big -F gay GREEN | lolcat
+toilet -f big -F gay DARGON | lolcat
+echo $bi"Gue tinggal Pulang Yah mas" $nama
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+sh TUANB4DUT..sh
+fi
+
+if [ $pilihan = 3 ]
+then
+clear
+figler BAHAN2 | lolcat
+echo "Tunggu Dulu Bahannya Masih proses"
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pkg install figlet
+pkg install termocolor
+pkg install pip2
+pip2 install php
+pkg install php
+pkg install update
+pkg install upgrade
+pkg upgrade
+pkg update
+sleep 2
+sh toolsv7.sh
+fi
+
+if [ $pilihan = 4 ]
+then
+clear
+figlet GREEN | lolcat
+figlet DRAGON | lolcat
+echo $bi"gue makan dulu yah bro :v"
+sleep 1
+echo $pu"username : BAJINGAN" $i"DI INGAT YAHH"
+sleep 3
+echo $pu"PASWORD : Gans" $i"Di ingat ingat"
+sleep 4
+pkg install python
+pkg install python
+pkg install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $pilihan = 0 ]
+then
+clear
+echo $me"Makasih Telah memakai Tools kami"
+sleep 1
+echo $bi"Sampai jumpa Lagi Kawan Suportme"
+sleep 1
+echo $ku"Subscribe chanek Bigbps Starla pliss"
+sleep 1
+echo $i"thx too"
+sleep 1
+echo $pu"Friends all cyber"
+sleep 1
+echo $cy"cyb3r pop1ce +62"
+sleep 1
+echo $me"arul-hanz-mr.ghost-dengklong-all team"
+sleep 1
+echo $me"Makasih Yah "$nama
+sleep 2
+exit
+fi
+
